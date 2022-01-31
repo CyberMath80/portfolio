88cdc556ce05177f2a0590d9a2f465af0381e0f0
eraste.mathieu@gmail.com <eraste.mathieu@gmail.com>
2022-01-31 14:32:16 +0100

    ./assets/js/concat.js -> add loop for currentYear

--- assets/js/concat.js
+++ assets/js/concat.js
@@ -1,8 +1,8 @@
 const phone   = document.querySelectorAll('.data-p');
 const email   = document.querySelectorAll('.data-e');
 
-const current = document.querySelector('.year');
-const year    = new Date();
+const currentYear = document.querySelectorAll('.year');
+const year        = new Date();
 
 for(let i = 0; i < phone.length; i++) {
     phone[i].innerHTML = '<a href="tel:+32477388045">+32 (0) 477 388 045</a>';
@@ -12,4 +12,6 @@ for(let i = 0; i < phone.length; i++) {
     email[i].innerHTML = '<a href="mailto:contact@cybermath.dev">contact@cybermath.dev</a>';
 }
 
-current.textContent = year.getFullYear();
+for(let i = 0; i < currentYear.length; i++) {
+    currentYear[i].textContent = year.getFullYear();
+}
