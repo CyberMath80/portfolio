905ab8b9be4a1ee756fecea6428af5baa1dfae78
eraste.mathieu@gmail.com <eraste.mathieu@gmail.com>
2022-01-31 05:39:56 +0100

    assets/js/form.js -> xhr

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,49 +1,30 @@
-const BTN_SUBMIT = document.getElementById('btn-submit');
+const FORM       = document.getElementById('contact-form');
 
-BTN_SUBMIT.addEventListener('click', () => {
-    sendForm();
+FORM.addEventListener('submit', function(e) {
+	e.preventDefault();
+
+	let data = new FormData(this);
+
+	let xhr = new XMLHttpRequest();
+
+	xhr.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			console.log(this.response);
+			let res = this.response;
+			if (res.success) {
+				console.log('Success !');
+			} else {
+				console.log(res.msg);
+			}
+		} else if (this.readyState == 4) {
+			console.log('Une erreur est survenue...');
+		}
+	};
+
+	xhr.open('POST', '../../contact.php', true);
+	xhr.responseType = 'json';
+	// xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.send(data);
+
+	return false;
 });
-
-function sendForm()
-{
-    // Récupération des valeurs des champs de formulaire
-    let post_id = document.getElementById('post_id').value;
-    let name    = document.getElementById('name').value;
-    let email   = document.getElementById('email').value;
-    let subject = document.getElementById('subject').value;
-    let message = document.getElementById('message').value;
-
-    // Préparation de la querystring d'URL
-    let params = '?post_id=' + encodeURIComponent(post_id);
-    params += '&name=' + encodeURIComponent(name);
-    params += '&email=' + encodeURIComponent(email);
-    params += '&subject=' + encodeURIComponent(subject);
-    params += '&message=' + encodeURIComponent(message);
-
-// Récupération de l'objet XHR
-let xhr = new XMLHttpRequest();
-
-// On assigne une fonction qui, lorsque l'état de la requête change, va traiter le résultat
-xhr.onreadystatechange = function() {
-    // readyState 4 = requête terminée
-    if (xhr.readyState == 4) {
-        // status 200 = page requêtée trouvée
-        if (xhr.status == 200)
-            ajaxBox_setText(xhr.responseText);
-        // Page non trouvée
-        else
-            ajaxBox_setText('Error...');
-    }
-};
-
-// Passage des paramètres à l'URL puis éxecution de la requête
-let url = 'https://cybermath.dev/forms/contact.php' + params;
-xhr.open('GET', url, true);
-xhr.send(null);
-}
-
-// Fonction de mise à jour du contenu de l'élement HTML #result
-function ajaxBox_setText(pText) {
-    let p = document.getElementById('result');
-    p.appendChild(document.createTextNode(pText));
-}
