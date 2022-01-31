6a4098c3022db43910d4cf0ece25083cc740d1bc
eraste.mathieu@gmail.com <eraste.mathieu@gmail.com>
2022-01-31 05:57:29 +0100

    assets/js/form.js -> preventDefault

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,30 +1,40 @@
-const FORM       = document.getElementById('contact-form');
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    let data = new FormData(this);
+    let xhr = createInstance();
 
-FORM.addEventListener('submit', function(e) {
-	e.preventDefault();
+    xhr.onreadystatechange = function() {
+        console.log(this);
+        if(this.readyState === 4 && this.status === 200) {
+            console.log(this.response);
+        } else if(this.readyState === 4) {
+            console.log('Error !');
+        }
+    };
 
-	let data = new FormData(this);
-
-	let xhr = new XMLHttpRequest();
-
-	xhr.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			console.log(this.response);
-			let res = this.response;
-			if (res.success) {
-				console.log('Success !');
-			} else {
-				console.log(res.msg);
-			}
-		} else if (this.readyState == 4) {
-			console.log('Une erreur est survenue...');
-		}
-	};
-
-	xhr.open('POST', '../../contact.php', true);
-	xhr.responseType = 'json';
-	// xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	xhr.send(data);
-
-	return false;
+    xhr.open('POST', '/contact.php', true);
+    xhr.responseType = 'json';
+    //xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send(data);
+    return false;
 });
+
+function createInstance() {
+    let req = null;
+    if(window.XMLHttpRequest)
+        req = new XMLHttpRequest();
+
+    else if(window.ActiveXObject) {
+    try {
+        req = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch(e) {
+        try {
+            req = new ActiveXObject("Microsoft.XMLHTTP");
+        } catch(e) {
+            alert("XHR not created");
+            }
+        }
+    }
+    return req;
+}
